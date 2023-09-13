7797cd57e28f38ca052a1366e78829da582a977a
Coolinar <orl.vasily@gmail.com>
2023-09-13 19:34:07 +0500

    7 task. First iteration. only 1 example pass and log solutions

--- SummerChallenge2023/Program.cs
+++ SummerChallenge2023/Program.cs
@@ -7,25 +7,9 @@ namespace SummerChallenge2023
     {
         static void Main(string[] args)
         {
-            var list = new List<List<int>>()
-            {
-                new() { 9, 7 },
-                new() { 6, 0 },
-                new() { 5, 6 },
-                new() { 9, 0 },
-                new() { 2, 8 },
-                new() { 4, 5 },
-                new() { 6, 8 },
-                new() { 3, 6 },
-                new() { 9, 1 },
-            };
-            var invalid = new List<List<int>>
-            {
-                new() { 2, 1 },
-                new() { 0, 2 },
-                new() { 0, 1 },
-            };
-            Console.WriteLine(string.Join(',',ExploitingSuperpowers_5.GearBalance(10, list)));
+            Console.WriteLine(Exploiting_superpowers_7.MixWishes("ish for laser eyes pleasei w", "i wish for laser telepathy please"));
+            // var mixWishes = Exploiting_superpowers_7.MixWishes("laser eyes", "telepathy");
+            // Console.WriteLine($"{mixWishes}: {mixWishes == "ltaselr epathyes"}");
         }
         
         
