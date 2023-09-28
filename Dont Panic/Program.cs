9f6a7843d9c5820b8b4e3aa79bcae3d926531de7
Coolinar <orl.vasily@gmail.com>
2023-09-28 13:43:58 +0500

    around 260 characters

--- Dont Panic/Program.cs
+++ Dont Panic/Program.cs
@@ -1,23 +1,11 @@
 ﻿using System;
 
-var r = P();
+var r=P();
+var n= I(7) + 1;
+var e= new int[n + 1];
+for (e[n] = I(4); n-- > 1; e[I(0) + 1] = I(1))P();
 
-var (x,n) = (I(4),I(7));
+for (;;Console.WriteLine((I(1) - e[I(0) + 1]) * (r[2][0] - 80) <= 0 ? "WAIT" : "BLOCK")) P();
 
-var e = new int[n+1];
-e[n] = x;
-for (var i = 0; i < n; i++)
-{
-    P();
-    e[I(0)] = I(1);
-}
-
-for(;;)
-{
-    P();
-    var (f,p, d) = (I(0),I(1), r[2][0]=='R'?1:-1);
-    Console.WriteLine((p-e[f==-1?n:f])*d<=0&f>=0? "WAIT" : "BLOCK");
-}
-
-string[]P() => r = Console.ReadLine().Split(' ');
+string[] P() => r = Console.ReadLine().Split(' ');
 int I(int i) => int.Parse(r[i]);
